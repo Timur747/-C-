c3b4738a79333d01839428395edafc91226da1d8
timur <tim.salixoff@yandex.ru>
2022-12-03 00:46:49 +0600

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 14: Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = 7;
+int c = 23;
+if(a % x == 0 && a % c == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
