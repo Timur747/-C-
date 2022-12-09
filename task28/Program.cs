1f48d1b96917c85690d1d7db8d77f18a5024ab05
timur <tim.salixoff@yandex.ru>
2022-12-10 00:49:41 +0600

    task30

---
+++ task28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+// Задача 28: Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+//возвращает сумму от 1 до number
+int GetProduct(int number)
+{
+    int Product = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        Product = Product * i;
+    }
+    return Product;
+}
+
+int A = ReadInt("Введите число: ");
+int result = GetProduct(A);
+Console.WriteLine($"Произведение чисел от 1 до {A} = {result}");
