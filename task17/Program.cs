24d3efbc1f691df65e3f5027ac32bcbd7986cf07
timur <tim.salixoff@yandex.ru>
2022-12-10 23:02:42 +0600

    task17

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,41 @@
 ﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
 // причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+Console.WriteLine("Введите координаты точек X и Y через запятую");
+string[] array = Console.ReadLine().Split(", ");
+int X = Convert.ToInt32(array[0]);
+int Y = Convert.ToInt32(array[1]);
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine(1);
+}
+if(X < 0 && Y > 0)
+{
+    Console.WriteLine(2);
+}
+if(X < 0 && Y < 0)
+{
+    Console.WriteLine(3);
+}
+if(X > 0 && Y < 0)
+{
+    Console.WriteLine(4);
+}
+if(X == 0 || Y == 0)
+{
+    Console.WriteLine("Введите иные координаты Х или У");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
